83331e414aa44d26a14739f9ced6ab87ac8f3340
akenoxd <akeno7@mail.ru>
2025-07-18 19:50:10 +0700

    done 09

--- 09_vfs/p1/main.c
+++ 09_vfs/p1/main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 int main() {
   const char *filename = "output.txt";
@@ -21,29 +20,19 @@ int main() {
   }
 
   fseek(fp, 0, SEEK_END);
-  long filesize = ftell(fp);
+  int filesize = ftell(fp);
   if (filesize < 0) {
     printf("ftell error\n");
     fclose(fp);
     return 1;
   }
-  char *buf = malloc(filesize + 1);
-  if (!buf) {
-    printf("malloc error");
-    fclose(fp);
-    return 1;
-  }
 
-  fseek(fp, 0, SEEK_SET);
-  fread(buf, 1, filesize, fp);
-  buf[filesize] = '\0';
-  fclose(fp);
-
-  // Print string in reverse
-  for (long i = filesize - 1; i >= 0; --i) {
-    putchar(buf[i]);
+  for (int i = filesize - 1; i >= 0; i--) {
+    fseek(fp, i, SEEK_SET);
+    putchar(fgetc(fp));
   }
   putchar('\n');
-  free(buf);
+
+  fclose(fp);
   return 0;
 }
